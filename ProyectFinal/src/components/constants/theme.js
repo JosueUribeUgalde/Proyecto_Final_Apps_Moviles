e951bc736f90c510510db8dbc3259ded327d3f5a
Josue uribe ugalde <josue86443@gmail.com>
2025-08-20 09:03:46 -0600

    feat:Agegado de arquitectura

---
+++ ProyectFinal/src/components/constants/theme.js
@@ -0,0 +1,49 @@
+export const COLORS = {
+    primary: '#1282ED',
+    secondary: '#ffffff',
+    background: '#dfe6e9',
+    backgroundBP: '#1282ED',
+    backgroundBS: '#ffffff',
+    textBlack: '#333333',
+    textWhite: '#ffffff',
+    borderGray: 'rgba(178, 190, 195, 0.4)',
+    error: '#e53935',
+};
+
+export const FONTS = {
+    small: 14,
+    regular: 16,
+    large: 18,
+    big: 28,
+};
+
+export const RADIUS = {
+    sm: 5,
+    md: 10,
+    lg: 20,
+};
+
+export const SHADOWS = {
+    light: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 2, // Esto solo va a aplicar en Android
+    },
+    medium: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 4,
+    },
+    dark: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 6 },
+        shadowOpacity: 0.3,
+        shadowRadius: 8,
+        elevation: 6,
+    },
+};
+
