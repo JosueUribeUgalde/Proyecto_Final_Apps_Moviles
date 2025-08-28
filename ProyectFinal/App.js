68aa1f5bd47b904abca9f6b9516bfa3c05e22e2f
ErickHub192 <earriola18@alumnos.uaq.mx>
2025-08-28 16:16:46 -0600

    feat: password reset functionality

--- ProyectFinal/App.js
+++ ProyectFinal/App.js
@@ -1,11 +1,12 @@
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
+import PasswordReset from "./src/screens/PasswordReset";
 
 
 export default function App() {
   return (
   
-      <Register/>
+      <PasswordReset/>
  
   );
 }
